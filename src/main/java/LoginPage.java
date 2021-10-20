6e46f9e538b3fe2552c2d0efd2ed1f5422394aab
Stanislav <kaplanst@mail.ru>
2021-10-20 17:42:37 -0400

    Created Second test class

--- src/main/java/LoginPage.java
+++ src/main/java/LoginPage.java
@@ -11,9 +11,10 @@ public class LoginPage {
     private By loginField = By.xpath("//input[@name='email']");
     private By passwordField = By.xpath("//input[@name='password']");
     private By submitButton = By.xpath("//input[@class='oct-button']");
-    private By registeredText = By.xpath("//*[text()='Я уже зарегистрирован']");
-    private By error = By.xpath("//*[@class='alert alert-danger']");
     private By regisrButton = By.xpath("//a[@class='oct-button']");
+
+    private By registeredText = By.xpath("//*[text()='Я уже зарегистрирован']");
+    private By incorrectRegisteredData = By.xpath("//*[text()=' Неправильно заполнены поля E-Mail и/или пароль!']");
 
 
     public LoginPage inputUserName(String username){
@@ -34,7 +35,7 @@ public class LoginPage {
         return driver.findElement(registeredText).getText();
     }
     public String getErrorText() { //error text when login or password incorrect
-        return driver.findElement(error).getText();
+        return driver.findElement(incorrectRegisteredData).getText();
     }
     public SignUpPage createAccount() { // redirecting to create account page
         driver.findElement(regisrButton).click();
--- src/test/java/LoginPageTest.java
+++ src/test/java/LoginPageTest.java
@@ -1,15 +1,17 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
 public class LoginPageTest {
 
     WebDriver driver;
-    MainPage mainPage;
+    LoginPage loginPage;
 
     @BeforeMethod
     public void SetUp(){
@@ -18,8 +20,24 @@ public class LoginPageTest {
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(5000, TimeUnit.MILLISECONDS);
         driver.get("https://vkitae.kz/login/");
-        LoginPage loginPage = new LoginPage(driver);
+        loginPage = new LoginPage(driver);
     }
+    @Test
+    public void loginWithEmptyCreds(){
+        loginPage.incorrectLoginCreds("", "");
+        Assert.assertEquals(loginPage.getErrorText(), "Неправильно заполнены поля E-Mail и/или пароль!");
+    }
+    @Test
+    public void loginWithIncorrectCreds(){
+        loginPage.incorrectLoginCreds("DDDDD", "DDDDD");
+        Assert.assertEquals(loginPage.getErrorText(), "Неправильно заполнены поля E-Mail и/или пароль!");
+    }
+    @Test
+    public void signUpPageRedirectTest(){
+        SignUpPage signUpPage = loginPage.createAccount();
+        Assert.assertEquals(signUpPage.getHeaderText(), "Быстрая регистрация");
+    }
+
 
     @AfterMethod
     public void Bye(){
