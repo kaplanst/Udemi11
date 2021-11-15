63ce544a943ed5c12d5797b4a81e9a342e435029
kaplanst <kaplanst@mail.ru>
2021-11-14 23:25:08 -0500

    Main page finish1

--- src/test/java/SignUpPageTest.java
+++ src/test/java/SignUpPageTest.java
@@ -1,4 +1,5 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.*;
 
@@ -17,19 +18,25 @@ public class SignUpPageTest extends BaseTest{
         driver.findElement(By.xpath("//a[@id='simpleregister_button_confirm']")).click();
         Assert.assertEquals(driver.findElement(By.xpath("//*[@id='pp_phone-error']")).getText(), "Такой номер уже зарегистрирован!\n" + "×");
     }
-//    @Test
-//    void confirmPassFieldTest(){
-//        signUpPage.inpuConfirmPassword(WRONG_PASSWORD);
-//        Assert.assertEquals(signUpPage.passwordConfirmErrorText(), "Подтверждение пароля не соответствует паролю!");
-//    }
-//    @Test
-//    void signUpEmptyPhoneTest(){
-//        signUpPage.registrationWithInvalidCreds("", WRONG_PASSWORD);
-//        Assert.assertEquals(signUpPage.phoneErrorText(), "Номер телефона указан не верно!");
-//    }
-//    @Test
-//    void signUpInvalidPasswordTest(){
-//        signUpPage.registrationWithInvalidCreds(LOGIN, SHORT_PASSWORD);
-//        Assert.assertEquals(signUpPage.passwordErrorText(), "Пароль должен быть от 4 до 20 символов!");
-//     }
+    @Test
+    void confirmPassFieldTest(){
+        driver.findElement(By.xpath("//input[@id='register_confirm_password']")).sendKeys(WRONG_PASSWORD);
+        String errorText = driver.findElement(By.xpath("//div[@class='simplecheckout-error-text simplecheckout-rule'][@data-equal='register_password']")).getText();
+        Assert.assertEquals(errorText, "Подтверждение пароля не соответствует паролю!");
+    }
+    @Test
+    void signUpEmptyPhoneTest(){
+        driver.findElement(By.xpath("//input[@id='register_password']")).sendKeys(WRONG_PASSWORD);
+        driver.findElement(By.xpath("//input[@id='register_confirm_password']")).sendKeys(WRONG_PASSWORD);
+        driver.findElement(By.xpath("//a[@id='simpleregister_button_confirm']")).click();
+        String errorPhone = driver.findElement(By.xpath("//div[@class='simplecheckout-error-text simplecheckout-rule'][@data-length-min='11']")).getText();
+        Assert.assertEquals(errorPhone, "Номер телефона указан не верно!");
+    }
+    @Test
+    void signUpInvalidPasswordTest(){
+        driver.findElement(By.xpath("//input[@id='register_telephone']")).sendKeys(LOGIN);
+        driver.findElement(By.xpath("//input[@id='register_password']")).sendKeys(SHORT_PASSWORD);
+        driver.findElement(By.xpath("//a[@id='simpleregister_button_confirm']")).click();
+        Assert.assertEquals(driver.findElement(By.xpath("//div[@class='simplecheckout-error-text simplecheckout-rule'][@data-length-min='4']")).getText(), "Пароль должен быть от 4 до 20 символов!");
+     }
 }
