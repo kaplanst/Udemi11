22e06725e6430873b6d20ae6aaff9840e913d229
Stanislav <kaplanst@mail.ru>
2021-11-24 17:56:14 -0500

    All tests updated3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,27 +3,21 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.GeckoDriverService;
+import org.openqa.selenium.opera.OperaDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         WebDriver driver;
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.get("https://vkitae.kz");
-        String mainTab = driver.getWindowHandle();
-        List<WebElement> right = driver.findElements(By.xpath("//*[@class='col-md-2 social-box']/ul/li"));
-        for (WebElement menu: right) {
-            menu.click();
-            for (String tab : driver.getWindowHandles()) {
-                driver.switchTo().window(tab);
-            }
-            System.out.println(driver.getCurrentUrl());
-            driver.close();
-            driver.switchTo().window(mainTab);
-        }
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//*[@id=\"oct-bluring-box\"]/footer/div/div[2]/div[3]/ul/li[1]/a")).click();
+
     }
 }
